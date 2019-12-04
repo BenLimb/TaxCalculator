f9ea13b3c9ced3b089fd699e15c9e820b765dcd7
BenLimb <54353133+BenLimb@users.noreply.github.com>
2019-12-04 11:54:55 +0000

    Test Upload

---
+++ Tax Calculator/Tax Calculator/LoginSignUp.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Tax_Calculator
+{
+    partial class LoginSignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameTxtBx = new System.Windows.Forms.TextBox();
+            this.passwordTxtBx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usernameTxtBx
+            // 
+            this.usernameTxtBx.Location = new System.Drawing.Point(25, 103);
+            this.usernameTxtBx.Name = "usernameTxtBx";
+            this.usernameTxtBx.Size = new System.Drawing.Size(159, 20);
+            this.usernameTxtBx.TabIndex = 1;
+            this.usernameTxtBx.Text = "Enter your Username here";
+            // 
+            // passwordTxtBx
+            // 
+            this.passwordTxtBx.Location = new System.Drawing.Point(25, 167);
+            this.passwordTxtBx.Name = "passwordTxtBx";
+            this.passwordTxtBx.Size = new System.Drawing.Size(159, 20);
+            this.passwordTxtBx.TabIndex = 2;
+            this.passwordTxtBx.Text = "Enter your Password here";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 41);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Login/Signup";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // LoginSignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(208, 319);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordTxtBx);
+            this.Controls.Add(this.usernameTxtBx);
+            this.Name = "LoginSignUp";
+            this.Text = "Login/SignUp";
+            this.Load += new System.EventHandler(this.LoginSignUp_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox usernameTxtBx;
+        private System.Windows.Forms.TextBox passwordTxtBx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Tax Calculator/Tax Calculator/LoginSignUp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tax_Calculator
+{
+    public partial class LoginSignUp : Form
+    {
+
+        public LoginSignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginSignUp_Load(object sender, EventArgs e)
+        {
+            Text = "Login";
+            usernameTxtBx.GotFocus += (UsernameRemoveText);
+            usernameTxtBx.LostFocus += (UsernameAddText);
+            passwordTxtBx.GotFocus += (PasswordRemoveText);
+            passwordTxtBx.LostFocus += (PasswordAddText);
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UsernameRemoveText(object sender, EventArgs e)
+        {
+            if (usernameTxtBx.Text == "Enter your Username here")
+                usernameTxtBx.Text = "";
+        }
+
+        private void UsernameAddText(object sender, EventArgs e)
+        {
+            if (usernameTxtBx.Text.Length == 0)
+                usernameTxtBx.Text = "Enter your Username here";
+        }
+        private void PasswordAddText(object sender, EventArgs e)
+        {
+            if (passwordTxtBx.Text.Length == 0)
+                passwordTxtBx.Text = "Enter your Password here";
+        }
+        private void PasswordRemoveText(object sender, EventArgs e)
+        {
+            if (passwordTxtBx.Text == "Enter your Password here")
+                passwordTxtBx.Text = "";
+        }
+    }
+}
